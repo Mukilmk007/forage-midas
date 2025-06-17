1c5266be27cd44c93a7196d5d6763f6dc578de12
Mukil <smukil007@gmail.com>
2025-06-17 22:19:43 +0530

    Add TransactionRecord entity for transaction management

---
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -0,0 +1,47 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private UserRecord sender;
+
+    @ManyToOne
+    private UserRecord recipient;
+
+    private double amount;
+
+    private double incentive;
+
+    public TransactionRecord() {}
+
+    public TransactionRecord(UserRecord sender, UserRecord recipient, double amount, double incentive) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+        this.incentive = incentive;
+    }
+
+    // Getters and setters
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public UserRecord getSender() { return sender; }
+    public void setSender(UserRecord sender) { this.sender = sender; }
+
+    public UserRecord getRecipient() { return recipient; }
+    public void setRecipient(UserRecord recipient) { this.recipient = recipient; }
+
+    public double getAmount() { return amount; }
+    public void setAmount(double amount) { this.amount = amount; }
+
+    public double getIncentive() { return incentive; }
+    public void setIncentive(double incentive) { this.incentive = incentive; }
+}
